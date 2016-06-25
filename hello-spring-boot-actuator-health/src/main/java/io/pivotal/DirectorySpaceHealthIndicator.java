08d1b208998ad8f14f2708713735ba619dc89efe
Felipe Gutierrez <fgutierrezcruz@pivotal.io>
2016-06-25 13:12:34 -0600

    adding a new example for a custom health indicator

---
+++ hello-spring-boot-actuator-health/src/main/java/io/pivotal/DirectorySpaceHealthIndicator.java
@@ -0,0 +1,54 @@
+package io.pivotal;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health.Builder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DirectorySpaceHealthIndicator extends AbstractHealthIndicator {
+	private static final Logger log = LoggerFactory.getLogger(DirectorySpaceHealthIndicator.class);
+	private final File directory;
+	private final long thresholdBytes;
+
+	@Autowired
+	public DirectorySpaceHealthIndicator(@Value("${health.directory.path:${user.dir}}") String path,
+			@Value("${health.directory.threshold.bytes:5120}") long thresholdBytes) throws IOException {
+	
+		directory = new File(path);
+		this.thresholdBytes = thresholdBytes;
+	}
+
+	@Override
+	protected void doHealthCheck(Builder builder) throws Exception {
+		log.info("Checking space...");
+		long currentDirectorySize = getDirectorySize(directory);
+		
+		if (thresholdBytes > currentDirectorySize) {
+			log.info("Directory: " + directory.getAbsolutePath() + " looks goood.");
+			builder.up().withDetail("directory.name", directory.getAbsolutePath()).withDetail("directory.size", currentDirectorySize);
+		} else {
+			log.warn("Directory: " + directory.getAbsolutePath() + " has reach the " + thresholdBytes + " bytes threshold.");
+			builder.down().withDetail("directory.error", "You reached the threshold of " + thresholdBytes + " bytes from this Directory: " + directory.getAbsolutePath());
+		}
+
+	}
+
+	private long getDirectorySize(File directory) {
+		long length = 0;
+		for (File file : directory.listFiles()) {
+			if (file.isFile())
+				length += file.length();
+			else
+				length += getDirectorySize(file);
+		}
+		return length;
+	}
+
+}
